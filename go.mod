b46777eae94df2d1da2939daef698891ed890c15
wenwen <wenwen3@lenovo.com>
2020-03-04 22:54:51 +0800

    add service limit and health

--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module AWSNotifications
 
 go 1.12
 
-require (
-	github.com/aws/aws-lambda-go v1.14.0
-	github.com/aws/aws-sdk-go v1.29.15
-)
+require github.com/aws/aws-lambda-go v1.14.0
--- main.go
+++ main.go
@@ -5,12 +5,12 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"github.com/aws/aws-lambda-go/events"
-	"github.com/aws/aws-lambda-go/lambda"
 	"net/http"
 	"os"
 	"time"
 
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
 	//"github.com/aws/aws-sdk-go/aws"
 	//"github.com/aws/aws-sdk-go/aws/awserr"
 	//"github.com/aws/aws-sdk-go/aws/session"
@@ -21,19 +21,21 @@ type Section struct {
 	ActivitySubtitle string `json:"activitySubtitle"`
 	ActivityImage    string `json:"activityImage"`
 	Facts            []Fact `json:"facts"`
-	Markdown bool `json:"markdown"`
+	Markdown         bool   `json:"markdown"`
 }
 
 type Fact struct {
 	Name  string `json:"name"`
 	Value string `json:"value"`
 }
+
 type PotentialActionInput struct {
 	Type        string `json:"@type"`
 	ID          string `json:"id"`
 	IsMultiline bool   `json:"isMultiline"`
 	Title       string `json:"title"`
 }
+
 type PotentialActionAction struct {
 	Type   string `json:"@type"`
 	Name   string `json:"name"`
@@ -41,22 +43,55 @@ type PotentialActionAction struct {
 }
 
 type PotentialAction struct {
-	Type   string `json:"@type"`
-	Name   string `json:"name"`
-	Inputs []PotentialActionInput `json:"inputs"`
+	Type    string                  `json:"@type"`
+	Name    string                  `json:"name"`
+	Inputs  []PotentialActionInput  `json:"inputs"`
+	Targets []PotentialActionTarget `json:"targets"`
 	Actions []PotentialActionAction `json:"actions"`
 }
 
-type Message struct {
-	Type       string `json:"@type"`
-	Context    string `json:"@context"`
-	ThemeColor string `json:"themeColor"`
-	Summary    string `json:"summary"`
-	Sections   []Section `json:"sections"`
+type PotentialActionTarget struct {
+	OS  string `json:"os"`
+	URI string `json:"uri"`
+}
+
+type TeamsMessage struct {
+	Type             string            `json:"@type"`
+	Context          string            `json:"@context"`
+	ThemeColor       string            `json:"themeColor"`
+	Summary          string            `json:"summary"`
+	Sections         []Section         `json:"sections"`
 	PotentialActions []PotentialAction `json:"potentialAction"`
 }
 
-type LimitMessage struct {
+type Detail struct {
+	CheckName       string `json:"check-name"`
+	CheckItemDetail struct {
+		Status       string `json:"Status"`
+		CurrentUsage string `json:"Current Usage"`
+		LimitName    string `json:"Limit Name"`
+		Region       string `json:"Region"`
+		Service      string `json:"Service"`
+		LimitAmount  string `json:"Limit Amount"`
+	} `json:"check-item-detail"`
+	Status     string `json:"status"`
+	ResourceID string `json:"resource_id"`
+	UUID       string `json:"uuid"`
+	EventArn          string `json:"eventArn"`
+	Service           string `json:"service"`
+	EventTypeCode     string `json:"eventTypeCode"`
+	EventTypeCategory string `json:"eventTypeCategory"`
+	StartTime         string `json:"startTime"`
+	EventDescription  []struct {
+		Language          string `json:"language"`
+		LatestDescription string `json:"latestDescription"`
+	} `json:"eventDescription"`
+	AffectedEntities []struct {
+		EntityValue string `json:"entityValue"`
+	} `json:"affectedEntities"`
+}
+
+type SNSMessage struct {
 	Version    string        `json:"version"`
 	ID         string        `json:"id"`
 	DetailType string        `json:"detail-type"`
@@ -65,23 +100,10 @@ type LimitMessage struct {
 	Time       time.Time     `json:"time"`
 	Region     string        `json:"region"`
 	Resources  []interface{} `json:"resources"`
-	Detail     struct {
-		CheckName       string `json:"check-name"`
-		CheckItemDetail struct {
-			Status       string `json:"Status"`
-			CurrentUsage string `json:"Current Usage"`
-			LimitName    string `json:"Limit Name"`
-			Region       string `json:"Region"`
-			Service      string `json:"Service"`
-			LimitAmount  string `json:"Limit Amount"`
-		} `json:"check-item-detail"`
-		Status     string `json:"status"`
-		ResourceID string `json:"resource_id"`
-		UUID       string `json:"uuid"`
-	} `json:"detail"`
+	Detail     Detail         `json:"detail"`
 }
 
-func sendToWebhook(chatApplication string,webhook string,message Message) error {
+func sendToWebhook(chatApplication string, webhook string, message TeamsMessage) error {
 	b := new(bytes.Buffer)
 	json.NewEncoder(b).Encode(message)
 	res, err := http.Post(webhook, "application/json; charset=utf-8", b)
@@ -92,47 +114,99 @@ func sendToWebhook(chatApplication string,webhook string,message Message) error 
 	return nil
 }
 
+//func setMessage(interface{}) TeamsMessage {
+//	var message TeamsMessage
+//
+//	return TeamsMessage{}
+//}
+
 func handler(ctx context.Context, event events.SNSEvent) {
 	var chatApplication = os.Getenv("Chat_Application")
 	var webhookURL = os.Getenv("Chat_Webhook")
-	var limitMessage LimitMessage
+	var snsMessage SNSMessage
 	if len(event.Records) > 0 {
 		for _, record := range event.Records {
-			fmt.Print(record.SNS.Message)
-			err := json.Unmarshal([]byte(record.SNS.Message),&limitMessage)
+			var message TeamsMessage
+			err := json.Unmarshal([]byte(record.SNS.Message), &snsMessage)
 			if err != nil {
 				fmt.Print(err)
 			}
-			var message Message
-			message.Type = "MessageCard"
-			message.Context = "http://schema.org/extensions"
-			message.ThemeColor = "0076D7"
-			var section Section
-			message.Summary = limitMessage.Detail.CheckItemDetail.LimitName
-			section.Markdown = true
-			section.ActivityTitle = "Limit Monitor"
-			var fact Fact
-			fact.Name = "Service"
-			fact.Value = limitMessage.Detail.CheckItemDetail.Service
-			section.Facts = append(section.Facts,fact)
-			fact.Name = "CurrentValue"
-			fact.Value = limitMessage.Detail.CheckItemDetail.CurrentUsage
-			section.Facts = append(section.Facts,fact)
-			fact.Name = "LimitAmount"
-			fact.Value = limitMessage.Detail.CheckItemDetail.LimitAmount
-			section.Facts = append(section.Facts,fact)
-			var potentialAction PotentialAction
-			var potentialActionInput PotentialActionInput
-			var potentialActionAction PotentialActionAction
-			potentialAction.Type = "ActionCard"
-			potentialAction.Name = "More details"
-			potentialActionAction.Type = "OpenUri"
-			potentialActionAction.Target = "https://console.aws.amazon.com/trustedadvisor/home?region=us-east-1#/category/service-limits"
-			potentialAction.Inputs = append(potentialAction.Inputs,potentialActionInput)
-			potentialAction.Actions = append(potentialAction.Actions,potentialActionAction)
-			message.PotentialActions = append(message.PotentialActions,potentialAction)
-			message.Sections = append(message.Sections,section)
-			err = sendToWebhook(chatApplication,webhookURL,message)
+			if snsMessage.Source == "aws.health" {
+				message = TeamsMessage{
+					Type:             "MessageCard",
+					Context:          "http://schema.org/extensions",
+					ThemeColor:       "fff30b",
+					Summary:          snsMessage.DetailType,
+					Sections:         []Section{
+						{
+							ActivityTitle:    snsMessage.Detail.EventTypeCode,
+							ActivitySubtitle: snsMessage.Detail.EventTypeCategory,
+							ActivityImage:    "",
+							Facts:            []Fact{
+								{
+									Name:  "Description",
+									Value: snsMessage.Detail.EventDescription[0].LatestDescription,
+								},
+							},
+							Markdown:         true,
+						},
+					},
+					PotentialActions: nil,
+				}
+			} else if snsMessage.Source == "aws.trustedadvisor" {
+				taUri := fmt.Sprintf("https://console.aws.amazon.com/trustedadvisor/home?region=%s#/category/service-limits",snsMessage.Detail.CheckItemDetail.Region)
+				colorMap := map[string]string{
+					"Red": "d7000b",
+					"Yellow": "fff30b",
+				}
+				message = TeamsMessage{
+					Type:             "MessageCard",
+					Context:          "http://schema.org/extensions",
+					ThemeColor:       colorMap[snsMessage.Detail.CheckItemDetail.Status],
+					Summary:          snsMessage.Detail.CheckItemDetail.LimitName,
+					Sections:         []Section{
+						{
+							ActivityTitle:    "AWS service limit monitor",
+							ActivitySubtitle: snsMessage.Detail.CheckName,
+							ActivityImage:    "",
+							Facts:            []Fact{
+								{
+									Name:  "Service",
+									Value: snsMessage.Detail.CheckItemDetail.Service,
+								},
+								{
+									Name:  "Region",
+									Value: snsMessage.Detail.CheckItemDetail.Region,
+								},
+								{
+									Name: "CurrentValue",
+									Value: snsMessage.Detail.CheckItemDetail.CurrentUsage,
+								},
+								{
+									Name: "LimitAmount",
+									Value: snsMessage.Detail.CheckItemDetail.LimitAmount,
+								},
+							},
+							Markdown:         true,
+						},
+					},
+					PotentialActions: []PotentialAction{
+						{
+							Type:    "OpenUri",
+							Name:    "More details",
+							Inputs:  nil,
+							Targets: []PotentialActionTarget{
+								{
+									OS:  "default",
+									URI: taUri,
+								},
+							},
+							Actions: nil,
+						},
+					},
+				}
+			}
+			err = sendToWebhook(chatApplication, webhookURL, message)
 			if err != nil {
 				fmt.Print(err)
 			}
